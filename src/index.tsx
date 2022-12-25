7dff501543e6120b033f225aee3ea0f469d06fef
Aeri Jung <rjungdesign@gmail.com>
2022-12-25 12:03:34 -0500

    update print and screen

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import App from "./App";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "styles/Theme";
--- src/ui/Layout/index.tsx
+++ src/ui/Layout/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 
 type Props = {
@@ -7,21 +8,24 @@ type Props = {
   rightComp?: JSX.Element | React.ReactNode;
 };
 
+const Container = styled(Grid)(() => {
+  return {
+    backgroundColor: "#fff",
+    padding: 50,
+  };
+});
+
 function Layout({ leftComp, rightComp }: Props) {
   return (
     <>
-      <Grid
-        container
-        component="main"
-        style={{ backgroundColor: "#fff", padding: 80 }}
-      >
+      <Container container>
         <Grid item xs={false} sm={4} md={7} style={{ border: "solid 1px " }}>
           {leftComp}
         </Grid>
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           {rightComp}
         </Grid>
-      </Grid>
+      </Container>
     </>
   );
 }
