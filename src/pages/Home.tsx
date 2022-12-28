576968e12365a2371554c698cd7a5483aebf7404
Aeri Jung <rjungdesign@gmail.com>
2022-12-28 13:22:39 -0500

    create separate style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,77 +1,13 @@
 import Typewriter from "typewriter-effect";
 import Grid from "@mui/material/Grid";
-import { styled } from "@mui/system";
-import Typography from "@mui/material/Typography";
 
-import { colors, fontFace } from "constants/styles";
-
-const StyledName = styled("span")(({ theme }) => {
-  return {
-    fontFamily: fontFace.code,
-    fontWeight: 800,
-    letterSpacing: "-0.04em",
-    fontSize: "4.2em",
-    color: colors.blue,
-    textAlign: "right",
-
-    [theme.breakpoints.down("md")]: {
-      fontSize: "2.8em",
-      textAlign: "center",
-    },
-  };
-});
-
-const StyledNameBox = styled(Grid)(({ theme }) => {
-  return {
-    display: "flex",
-    justifyContent: "right",
-    flexDirection: "column",
-
-    [theme.breakpoints.down("md")]: {
-      justifyContent: "center",
-    },
-  };
-});
-
-const StyledAnimationTxt = styled("div")(({ theme }) => {
-  return {
-    textAlign: "right",
-    fontWeight: 800,
-    color: colors.blue,
-
-    "& .Typewriter__wrapper": {
-      width: "100%",
-      backgroundColor: colors.highlight,
-    },
-
-    [theme.breakpoints.down("md")]: {
-      textAlign: "center",
-    },
-  };
-});
-
-const StyledIntroBox = styled(Grid)(({ theme }) => {
-  return {
-    display: "flex",
-    alignItems: "center",
-
-    [theme.breakpoints.down("md")]: {
-      justifyContent: "center",
-    },
-  };
-});
-
-const StyledAbout = styled(Typography)(({ theme }) => {
-  return {
-    color: colors.range,
-    fontSize: 13,
-
-    [theme.breakpoints.down("md")]: {
-      textAlign: "center",
-      padding: 30,
-    },
-  };
-});
+import {
+  StyledName,
+  StyledNameBox,
+  StyledAnimationTxt,
+  StyledIntroBox,
+  StyledAbout,
+} from "styles/Home";
 
 const currentYear = new Date();
 
