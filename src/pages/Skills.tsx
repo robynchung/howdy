b64d9bd8e1af528fc0e6a50c8b936ee08c36fa62
Aeri Jung <rjungdesign@gmail.com>
2022-12-29 07:31:54 -0500

    add skill list data

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -3,60 +3,34 @@ import Title from "components/common/Title";
 
 import { StyledList } from "styles/Skills";
 
+import skills from "data/skills.json";
+
+type SkillType = {
+  title: string;
+  list: string[];
+};
+
 function Skills() {
+  const renderList = (list: Array<string>) => {
+    return list.map((item) => {
+      const replaced = item
+        .replace(/^\[/g, `<span class="highlight">`)
+        .replace(/\]$/g, `</span>`);
+
+      return <li dangerouslySetInnerHTML={{ __html: replaced }} />;
+    });
+  };
+
   return (
     <Grid container spacing={8}>
-      <Grid item md={4} xs={12}>
-        <Title>Frameworks & Technologies</Title>
-        <StyledList>
-          <li>
-            <span className="highlight">React / Redux / Recoil </span>
-          </li>
-          <li>
-            <span className="highlight">React Native</span>
-          </li>
-          <li>
-            <span className="highlight">Node.js</span>
-          </li>
-          <li>
-            <span className="highlight">Serverless framework</span>
-          </li>
-          <li>Google Firebase</li>
-          <li>Webpack</li>
-        </StyledList>
-      </Grid>
-
-      <Grid item md={4} xs={12}>
-        <Title>Languages & Tools</Title>
-        <StyledList>
-          <li>
-            <span className="highlight">Javascript(ES6+)</span>
-          </li>
-          <li>
-            <span className="highlight">Amazon AWS / DynamoDB / RDS</span>
-          </li>
-          <li>
-            <span className="highlight">Typescript</span>
-          </li>
-          <li>Python</li>
-
-          <li>HTML5 </li>
-          <li>CSS3 / SCSS</li>
-
-          <li>Firebase</li>
-        </StyledList>
-      </Grid>
-
-      <Grid item md={4} xs={12}>
-        <Title>Personality traits</Title>
-        <StyledList>
-          <li>Work independently</li>
-          <li>Self-Starter</li>
-          <li>Hard working</li>
-          <li>Team oriented </li>
-          <li>Adapt and learn rapidly evolving tech stack </li>
-        </StyledList>
-      </Grid>
+      {skills.map((skill: SkillType) => {
+        return (
+          <Grid item md={4} xs={12}>
+            <Title>{skill.title}</Title>
+            <StyledList>{renderList(skill.list)}</StyledList>
+          </Grid>
+        );
+      })}
     </Grid>
   );
 }
