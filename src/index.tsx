d4128fdffcfccf193c15b08bbf4a1432b1ad6e1d
Aeri Jung <rjungdesign@gmail.com>
2022-12-29 15:38:33 -0500

    add helmet and favicon

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+
+import { Helmet } from "react-helmet";
+
 import CssBaseline from "@mui/material/CssBaseline";
 
+import "./index.css";
+import App from "./App";
+import theme from "theme";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "theme";
+import seo from "data/seo.json";
+import seoImg from "assets/seo.png";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -18,6 +23,25 @@ console.log(process.env);
 root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
+    <Helmet titleTemplate={seo.titleTemplate} defaultTitle={seo.defaultTitle}>
+      <base target="_blank" href={seo.baseURL} />
+      <meta name="description" content={seo.description} />
+
+      {/* facebook */}
+      <meta property="og:title" content={seo.defaultTitle} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seoImg} />
+      <meta property="og:url" content={seo.baseURL} />
+      <meta property="og:site_name" content={seo.defaultTitle} />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content={seo.type} />
+
+      <meta name="twitter:card" content={seoImg} />
+      <meta name="twitter:title" content={seo.defaultTitle} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seoImg} />
+    </Helmet>
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
