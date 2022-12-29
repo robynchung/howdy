36b055e4c08b110cb1fb349dcdab9a694299e5a9
Aeri Jung <rjungdesign@gmail.com>
2022-12-29 12:09:49 -0500

    update contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,21 +7,34 @@ import { SubContainer } from "components/Layout/Container";
 import { StyledSubmitContainer } from "styles/Contact";
 import { StyledButton } from "styles/Menu";
 
+interface IFormInput {
+  fullName: string;
+  email: string;
+  message: string;
+}
+
 function Contact() {
   const {
     register,
     handleSubmit,
-    watch,
+
     formState: { errors },
   } = useForm<IFormInput>();
 
-  interface IFormInput {
-    fullName: string;
-    email: string;
-    message: string;
-  }
-
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+
+  const renderErrorMessage = (type: string | undefined): string => {
+    switch (type) {
+      case "required":
+        return "This field is required";
+
+      case "pattern":
+        return "This value doesn't meet requirement";
+
+      default:
+        return "";
+    }
+  };
 
   return (
     <SubContainer>
@@ -29,20 +42,28 @@ function Contact() {
         <Grid container spacing={3}>
           <Grid item xs={12}>
             <TextField
+              error={"fullName" in errors}
               fullWidth
               required
+              helperText={renderErrorMessage(errors?.fullName?.type)}
               label="Full Name"
               variant="filled"
-              {...register("fullName", { required: true, maxLength: 20 })}
+              {...register("fullName", { required: true })}
             />
           </Grid>
           <Grid item xs={12}>
             <TextField
+              error={"email" in errors}
               fullWidth
               required
               label="E-mail"
               variant="filled"
-              {...register("email", { required: true, maxLength: 20 })}
+              helperText={renderErrorMessage(errors?.email?.type)}
+              {...register("email", {
+                required: true,
+                pattern:
+                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/gi,
+              })}
             />
           </Grid>
           <Grid item xs={12}>
@@ -51,10 +72,10 @@ function Contact() {
               required
               label="Message"
               variant="filled"
-              maxRows={10}
               rows={10}
+              helperText={renderErrorMessage(errors?.message?.type)}
               multiline
-              {...register("message", { required: true, maxLength: 20 })}
+              {...register("message", { required: true })}
             />
           </Grid>
           <StyledSubmitContainer item xs={12}>
